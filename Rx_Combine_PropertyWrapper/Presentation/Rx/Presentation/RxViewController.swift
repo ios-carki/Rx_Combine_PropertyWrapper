b690e74ce00b94169a521aff1d4625c8477020c5
carki <94.nogari@gmail.com>
2023-07-13 00:09:53 +0900

    [Add] Rx_Swift - Simple Increase Decrease Number Func Added

--- Rx_Combine_PropertyWrapper/Presentation/Rx/Presentation/RxViewController.swift
+++ Rx_Combine_PropertyWrapper/Presentation/Rx/Presentation/RxViewController.swift
@@ -7,8 +7,17 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+
+
 final class RxViewController: UIViewController {
     private let mainView = RxView()
+    private let viewModel = RxViewModel()
+    private let disposeBag = DisposeBag()
+    
+    let observable = Observable<Int>.just(1)
+    var count = 0
     
     override func loadView() {
         view = mainView
@@ -17,5 +26,27 @@ final class RxViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        increaseClick()
+        decreaseClick()
+    }
+    
+    func increaseClick() {
+        print("숫자 증가")
+        mainView.increaseButton.rx.tap
+            .subscribe(onNext: {
+                self.count += 1
+                self.mainView.counterLabel.text = String(self.count)
+            })
+            .disposed(by: disposeBag)
+    }
+    
+    func decreaseClick() {
+        print("숫자 감소")
+        mainView.decreaseButton.rx.tap
+            .subscribe(onNext: {
+                self.count -= 1
+                self.mainView.counterLabel.text = String(self.count)
+            })
+            .disposed(by: disposeBag)
     }
 }
---
+++ Rx_Combine_PropertyWrapper/Presentation/Rx/Presentation/RxViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  RxViewModel.swift
+//  Rx_Combine_PropertyWrapper
+//
+//  Created by Carki on 2023/05/29.
+//
+
+import Foundation
+
+import RxSwift
+
+final class RxViewModel {
+    
+}
